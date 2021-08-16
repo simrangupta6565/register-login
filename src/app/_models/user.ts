7e6e988c26cdac6d1a8ae64d6ff00eabba282784
simrangupta6565 <simrangupta6565@gmail.com>
2021-08-16 08:21:57 +0530

    first commit

---
+++ src/app/_models/user.ts
@@ -0,0 +1,9 @@
+﻿export class User {
+    id: string;
+    name: string;
+    address: string;
+    phone: string;
+    username: string;
+    password: string;
+   token: string;
+}
---
+++ src/app/account/register.component.ts
@@ -0,0 +1,80 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AccountService, AlertService } from '@app/_services';
+
+import { ConfirmedValidator } from 'src/app/_helpers/confirmed.validator';
+@Component({ templateUrl: 'register.component.html' })
+export class RegisterComponent implements OnInit {
+    form: FormGroup;
+    loading = false;
+    submitted = false;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService,
+        private alertService: AlertService
+    ) { }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            name: ['', Validators.required],
+            address: ['', Validators.required],
+            phone: ['', Validators.required],
+            username: ['', Validators.required],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirm: ['', [Validators.required, this.passwordMatcher.bind(this)]]
+      
+        
+        });
+    }
+
+    // confirm new password validator
+private passwordMatcher(control: FormControl): { [s: string]: boolean } {
+    if (
+        this.form &&
+        (control.value !== this.form.controls.password.value)
+    ) {
+        return { passwordNotMatch: true };
+    }
+    return null;
+}
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.register(this.form.value)
+            .pipe(first())
+            .subscribe({
+                next: () => {
+                    this.alertService.success('Registration successful', { keepAfterRouteChange: true });
+                    this.router.navigate(['../login'], { relativeTo: this.route });
+                },
+                error: error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                }
+            });
+    }
+}
+
+// function matchValues(arg0: string): any {
+//     return this.form.password === this.form.confirm ? null : { passwordsNotEqual: true } 
+    
+// }
